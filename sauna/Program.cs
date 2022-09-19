c0b97e4c64603ef1afddf4a1084d194ee7b8aee7
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-09-19 17:03:57 +0200

    Add project files.

---
+++ sauna/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+namespace sauna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature = Run();
+            while (temperature < 73 || temperature > 77)
+                temperature = Run();
+        }
+        static double Run()
+
+        {
+            Console.Write("Enter temperature ");
+            double far = Int32.Parse(Console.ReadLine());
+            double cel = (far - 32) * 5 / 9;
+
+            {
+                if (cel < 73)
+                {
+                    Console.WriteLine("tempreture is too low");
+                }
+                else if (cel > 77)
+                {
+                    Console.WriteLine("tempreture is too high");
+                }
+                else
+                {
+                    Console.WriteLine("acceptable temperature");
+                }
+                return cel;
+            }
+        }
+    }
+}   
+ 
